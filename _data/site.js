f3cb631b931255432f439f96ee1855288643f31b
Brad Jeschonek <blj747@gmail.com>
2026-06-25 23:36:26 -0400

    dynamic year in footer

--- _data/site.js
+++ _data/site.js
@@ -1,5 +1,6 @@
 export default function () {
   return {
+    currentYear: new Date().getFullYear(),
     analytics: {
       src: process.env.ANALYTICS_SRC || "https://cloud.umami.is/script.js",
       websiteId: process.env.ANALYTICS_WEBSITE_ID || "",
